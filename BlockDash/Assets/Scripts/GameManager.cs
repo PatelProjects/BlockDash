371e78f2c7612aca3bfe31d42f505bbecaf75fd4
Abhishek Patel <38337132+PatelProjects@users.noreply.github.com>
2019-09-22 02:21:54 -0400

    Set up core object + complete level 1

---
+++ BlockDash/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public float restartDelay;
+
+    public GameObject completeLevelUI;
+
+    public void EndGame()
+    {
+        Debug.Log("Game Over");
+
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", restartDelay);
+        }
+
+
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+}
